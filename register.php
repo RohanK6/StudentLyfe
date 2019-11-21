518dd6be309280a5e38bd9e9ea59aa643c937c27
Rohan <rohan.khanderia@gmail.com>
2019-11-20 22:47:07 -0500

    Isolated register forms from log in forms

--- register.php
+++ register.php
@@ -1,6 +1,17 @@
 <?php 
-    require "header.php";
+    session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>StudentLyfe</title>
+    <link rel = "stylesheet" href = "styles.css">
+</head>
+<body>
 
     <main>
         <h1> Register </h1>
@@ -44,8 +55,11 @@
             <input type = "password" name = "uPwdRepeat" placeholder = "Repeat Password...">
             <button type = "submit" name = "register-submit"> Register </button>
         </form>
+        <a href = "index.php"> Already have an account? Log in here! </a>
     </main>
 
 <?php 
     require "footer.php";
 ?>
+</body>
+</html>
